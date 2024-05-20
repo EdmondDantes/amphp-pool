dcae5640c1f0281f2675a99dc09aa4025991f897
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-05-20 14:18:13 +0300

    + Add start.php module

--- start.php
+++ start.php
@@ -1,3 +1,12 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use CT\AmpServer\WorkerPool;
+use CT\AmpServer\WorkerEntryPoint;
+
+$workerPool                         = new WorkerPool(1, 0);
+$workerPool->fillWorkersWith(WorkerEntryPoint::class);
+$workerPool->run();
+$workerPool->mainLoop();
