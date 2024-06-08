ed3a644d072fb0be52a9e65605e7f16612c76f01
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-06-08 12:56:10 +0300

    + IpcClient

--- src/JobIpc/IpcClient.php
+++ src/JobIpc/IpcClient.php
@@ -11,6 +11,7 @@ use Amp\ForbidSerialization;
 use Amp\Future;
 use Amp\Socket\Socket;
 use Amp\TimeoutCancellation;
+use Amp\TimeoutException;
 use CT\AmpServer\Exceptions\NoAvailableWorkers;
 use CT\AmpServer\Exceptions\SendJobException;
 use CT\AmpServer\PoolState\PoolStateStorage;
@@ -27,23 +28,30 @@ final class IpcClient
     private JobTransportI|null $jobTransport      = null;
     private WorkersStateInfo|null $workersInfo    = null;
     private PoolStateStorage|null $poolState      = null;
-    private array $resultsFutures = [];
+    /**
+     * List of futures that are waiting for the result of the job with SocketId, and time when the job was sent
+     * @var array [Future, int, int]
+     */
+    private array $resultsFutures   = [];
     private int $maxTryCount        = 3;
+    private int $futureTimeout      = 60 * 10;
+    private string $futureTimeoutCallbackId;
     
     public function __construct(
         private readonly int $workerId,
         private readonly int $workerGroupId = 0,
         JobTransportI $jobTransport = null,
-        private Cancellation|null $cancellation = null
+        private readonly Cancellation|null $cancellation = null
     )
     {
-        if($this->cancellation === null) {
-            $this->cancellation     = new TimeoutCancellation(5);
-        }
-        
         $this->workersInfo          = new WorkersStateInfo();
         $this->poolState            = new PoolStateStorage();
         $this->jobTransport         = $jobTransport ?? new JobTransport();
+    }
+    
+    public function mainLoop(): void
+    {
+        $this->futureTimeoutCallbackId = EventLoop::repeat($this->futureTimeout / 2, $this->updateFuturesByTimeout(...));
     }
     
     public function sendJob(string $data, int $workerGroupId, bool $awaitResult = false, int $workerId = null): Future|null
@@ -67,10 +75,10 @@ final class IpcClient
         
         while($tryCount < $this->maxTryCount) {
             try {
-                $this->tryToSendJob($data, $workerGroupId, $workerId, $ignoreWorkers, $deferred);
+                $socketId           = $this->tryToSendJob($data, $workerGroupId, $workerId, $ignoreWorkers, $deferred);
                 
                 if($deferred !== null) {
-                    $this->resultsFutures[spl_object_id($deferred)] = $deferred;
+                    $this->resultsFutures[spl_object_id($deferred)] = [$deferred, $socketId, time()];
                     return $deferred->getFuture();
                 } else {
                     return null;
@@ -89,7 +97,7 @@ final class IpcClient
         throw new SendJobException($workerGroupId, $this->maxTryCount);
     }
     
-    private function tryToSendJob(string $data, int $workerGroupId, int $workerId = null, array $ignoreWorkers = [], DeferredFuture $deferred = null): void
+    private function tryToSendJob(string $data, int $workerGroupId, int $workerId = null, array $ignoreWorkers = [], DeferredFuture $deferred = null): int
     {
         $foundedWorkerId            = $this->pickupWorker($workerGroupId, $workerId, $ignoreWorkers);
         
@@ -106,10 +114,28 @@ final class IpcClient
             $deferred->complete($exception);
             throw $exception;
         }
+        
+        return spl_object_id($socket);
     }
     
     public function __destruct()
     {
+        $this->stop();
+    }
+    
+    public function stop(): void
+    {
+        EventLoop::cancel($this->futureTimeoutCallbackId);
+        
+        $sockets                    = $this->workerSockets;
+        $this->workerSockets         = [];
+        
+        foreach($sockets as $socket) {
+            try {
+                $socket->close();
+            } catch (\Throwable) {
+            }
+        }
     }
     
     private function pickupWorker(int $workerGroupId, int $workerId = null, array $ignoreWorkers = []): int|null
@@ -159,6 +185,8 @@ final class IpcClient
         
         $this->workerSockets[$workerId] = $this->createConnectToWorker($workerId);
         
+        EventLoop::queue($this->readLoop(...), $workerId);
+        
         return $this->workerSockets[$workerId];
     }
     
@@ -167,11 +195,48 @@ final class IpcClient
         $connector                  = socketConnector();
         
         $client                     = $connector->connect(
-            IpcServer::getSocketAddress($workerId), cancellation: $this->cancellation
+            IpcServer::getSocketAddress($workerId), cancellation: new TimeoutCancellation(5)
         );
         
         $client->write(IpcServer::HAND_SHAKE);
         
         return $client;
+    }
+    
+    private function readLoop(int $workerId): void
+    {
+        $socket                     = $this->workerSockets[$workerId] ?? null;
+        
+        if($socket === null) {
+            return;
+        }
+        
+        try {
+            while (($data = $socket->read($this->cancellation)) !== null) {
+                
+                $response           = $this->jobTransport->parseResponse($data);
+                
+                if(array_key_exists($response->jobId, $this->resultsFutures)) {
+                    [$deferred, ] = $this->resultsFutures[$response->jobId];
+                    unset($this->resultsFutures[$response->jobId]);
+                    $deferred->complete($response->data);
+                }
+            }
+        } catch (\Throwable $exception) {
+            unset($this->workerSockets[$workerId]);
+            $socket->close();
+        }
+    }
+    
+    private function updateFuturesByTimeout(): void
+    {
+        $currentTime                = time();
+        
+        foreach($this->resultsFutures as $id => [$deferred, $socketId, $time]) {
+            if($currentTime - $time > $this->futureTimeout) {
+                unset($this->resultsFutures[$id]);
+                $deferred->error(new TimeoutException('Future timeout: ' . $this->futureTimeout));
+            }
+        }
     }
 }
