963c90707905012c8663ab569307b18e51031fd7
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-06-10 20:36:53 +0300

    + test WorkerStateStorageTest

---
+++ tests/WorkerState/WorkerStateStorageTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace CT\AmpServer\WorkerState;
+
+use PHPUnit\Framework\TestCase;
+
+class WorkerStateStorageTest        extends TestCase
+{
+    public function testWriteRead(): void
+    {
+        $workerStateStorage         = new WorkerStateStorage(1, 1, true);
+        $workerStateStorageRead     = new WorkerStateStorage(1);
+        
+        $workerStateStorage->workerReady();
+        $workerStateStorageRead->update();
+        
+        $this->assertTrue($workerStateStorageRead->isWorkerReady(), 'Worker is ready');
+    }
+    
+    public function testWriteReadJobCount(): void
+    {
+        $workerStateStorage         = new WorkerStateStorage(1, 1, true);
+        $workerStateStorageRead     = new WorkerStateStorage(1);
+        
+        $workerStateStorage->workerReady();
+        $workerStateStorage->incrementJobCount();
+        $workerStateStorage->incrementJobCount();
+        $workerStateStorage->incrementJobCount();
+        $workerStateStorageRead->update();
+        
+        $this->assertEquals(3, $workerStateStorageRead->getJobCount(), 'Job count');
+    }
+    
+    public function testWriteReadJobCountDecrement(): void
+    {
+        $workerStateStorage         = new WorkerStateStorage(1, 1, true);
+        $workerStateStorageRead     = new WorkerStateStorage(1);
+        
+        $workerStateStorage->workerReady();
+        $workerStateStorage->incrementJobCount();
+        $workerStateStorage->incrementJobCount();
+        $workerStateStorage->incrementJobCount();
+        $workerStateStorage->decrementJobCount();
+        $workerStateStorageRead->update();
+        
+        $this->assertEquals(2, $workerStateStorageRead->getJobCount(), 'Job count');
+    }
+}
