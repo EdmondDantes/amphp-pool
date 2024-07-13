a4ff6772bcf1a683dd90b338afd9b2b7cdc6a457
EdmondDantes <ed@pne.io>
2024-07-13 11:43:06 +0300

    % add withTcpNoDelay option

--- examples/HttpServer/HttpReactor.php
+++ examples/HttpServer/HttpReactor.php
@@ -9,6 +9,7 @@ use Amp\Http\Server\Driver\SocketClientFactory;
 use Amp\Http\Server\RequestHandler\ClosureRequestHandler;
 use Amp\Http\Server\Response;
 use Amp\Http\Server\SocketHttpServer;
+use Amp\Socket\BindContext;
 use IfCastle\AmpPool\Worker\WorkerEntryPointInterface;
 use IfCastle\AmpPool\Worker\WorkerInterface;
 
@@ -50,7 +51,7 @@ final class HttpReactor implements WorkerEntryPointInterface
         $httpServer                 = new SocketHttpServer($worker->getLogger(), $socketFactory, $clientFactory);
 
         // 2. Expose the server to the network
-        $httpServer->expose('127.0.0.1:9095');
+        $httpServer->expose('127.0.0.1:9095', (new BindContext())->withTcpNoDelay());
 
         // 3. Handle incoming connections and start the server
         $httpServer->start(
